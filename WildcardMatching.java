727073493084f032a0a451d6a71fadba767d3e96
Arthur Itajahy <canalp3rry@gmail.com>
2024-04-22 20:03:16 -0300

    Exec: Wildcard Matching

---
+++ WildcardMatching.java
@@ -0,0 +1,35 @@
+public class WildcardMatching {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else if (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+    public static void main(String[] args) {
+        WildcardMatching wildcardMatching = new WildcardMatching();
+
+        // Test cases
+        System.out.println(wildcardMatching.isMatch("aa", "a"));  // Output: false
+        System.out.println(wildcardMatching.isMatch("aa", "*"));  // Output: true
+        System.out.println(wildcardMatching.isMatch("cb", "?a")); // Output: false
+    }
+}
