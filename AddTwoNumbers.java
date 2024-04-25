28123d165567e1f35854e3a2db8227398e07fae2
Arthur Itajahy <canalp3rry@gmail.com>
2024-04-25 11:06:42 -0300

    Exec: Add Two Numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -15,31 +15,80 @@ class ListNode {
 }
 
 public class AddTwoNumbers {
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
+
+        // Reverse linked lists
+        ListNode reversedL1 = reverseList(l1);
+        ListNode reversedL2 = reverseList(l2);
+
+        // Transform into string
+        String numberl1 = listToString(reversedL1);
+        String numberl2 = listToString(reversedL2);
+
+        // Perform Addition
+        String sum = addString(numberl1, numberl2);
+
+
+        // Turn sum into list
+        ListNode sumList = stringToList(sum);
+
+        // Reverse the sum list
+        ListNode result = reverseList(sumList);
+
+        return result;
+    }
+
+    public static ListNode stringToList(String number) {
+        ListNode dummyHead = new ListNode();
         ListNode current = dummyHead;
-        int carry = 0;
-
-        while (l1 != null || l2 != null) {
-            int x = (l1 != null) ? l1.val : 0;
-            int y = (l2 != null) ? l2.val : 0;
-
-            int sum = x + y + carry;
-            carry = sum / 10;
-            current.next = new ListNode(sum % 10);
+        String strNumber = String.valueOf(number);
+        for (int i = 0; i < strNumber.length(); i++) {
+            current.next = new ListNode(Character.getNumericValue(strNumber.charAt(i)));
             current = current.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
         }
-
-        if (carry > 0) {
-            current.next = new ListNode(carry);
-        }
-
         return dummyHead.next;
     }
 
+
+    public static String addString(String num1, String num2){
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        while(i >= 0 || j >= 0 || carry != 0){
+            int digit1 = i >= 0 ? Character.getNumericValue(num1.charAt(i)) : 0;
+            int digit2 = j >= 0 ? Character.getNumericValue(num2.charAt(j)) : 0;
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10;
+            sb.append(sum % 10);
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
+    public static String listToString(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        ListNode current = head;
+        while (current != null) {
+            sb.append(current.val);
+            current = current.next;
+        }
+        return sb.toString();
+    }
     public void printList(ListNode head) {
         ListNode current = head;
         while (current != null) {
